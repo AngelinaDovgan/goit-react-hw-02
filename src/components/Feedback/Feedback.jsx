4c7de92f39885c586d8161c7c5b0bfc5aa1b9582
Angelina <midnightshout03@gmail.com>
2024-04-04 09:17:38 +0300

    update the name of p

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,7 +4,7 @@ export default function Feedback({ value: { good, neutral, bad }, summary }) {
             <p>Good: {good}</p>
             <p>Bad: {bad}</p>
             <p>Neutral: {neutral}</p>
-            <p>Result: {summary}</p>
+            <p>Positive: {summary}</p>
         </div>
     )
 }
